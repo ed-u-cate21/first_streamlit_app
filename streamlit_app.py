fecc8ae2a2fc9f34893c70648726094616e41331
MellyMel MeInyk <115183850+ed-u-cate21@users.noreply.github.com>
2022-10-06 15:33:30 -0400

    Update streamlit_app.py
    Make the selection fruit names

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # packages to import
 import streamlit 
-import pandas
+
 
 # Build the menu items
 streamlit.header('Breakfast Favorites')
@@ -10,9 +10,13 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+#import pandas
+import pandas 
+
 # Build a fruit chart from csv on aws
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
 
 #Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
