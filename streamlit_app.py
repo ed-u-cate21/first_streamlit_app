90daf3d26b49a11faf50fbd285f2855fb2b91cb4
MellyMel MeInyk <115183850+ed-u-cate21@users.noreply.github.com>
2022-10-06 15:02:22 -0400

    Update streamlit_app.py
    fixed the quotes to capture the emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text(🥣'Omega 4 & Blueberry Oatmeal')
-streamlit.text(🥗'Kale, Spinach & Rocket Smoothie')
-streamlit.text(🐔'Hard-Boiled Free-Range Egg')
-streamlit.text(🥑🍞 'Avocado Toast')
+streamlit.text('🥣Omega 4 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
