192aeab3b6a683be3e344f34a48f306acfda8336
MellyMel MeInyk <115183850+ed-u-cate21@users.noreply.github.com>
2022-10-06 15:16:27 -0400

    Update streamlit_app.py
    deleted for example

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
+# packages to import
 import streamlit 
 import pandas
 
+# Build the menu items
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 4 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
@@ -8,6 +10,5 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+# Build a fruit chart from csv on aws
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
