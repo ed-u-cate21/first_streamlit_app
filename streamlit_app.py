d77f971d7db570066143f4cbabfc3836c26c5fbb
MellyMel MeInyk <115183850+ed-u-cate21@users.noreply.github.com>
2022-10-07 11:22:55 -0400

    Update streamlit_app.py
    create fruit option and have the API return that information

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,12 +30,17 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display the fruityvice API response 
 streamlit.header('Fruityvice Fruit Advice')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 
 ## Clean the text up for normalized text display
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Creates the results into a dataframe table
 streamlit.dataframe(fruityvice_normalized)
+
+
+
