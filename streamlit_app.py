15c89724ac17ba816d895de9697f2b8c38371d53
MellyMel MeInyk <115183850+ed-u-cate21@users.noreply.github.com>
2022-10-07 11:21:04 -0400

    Update streamlit_app.py
    deleted the streamlit.text because it is being repeated down in the normalized text display 
    
    added kiwi as an option

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,8 +32,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice')
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+
 
 ## Clean the text up for normalized text display
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
