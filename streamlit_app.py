7b80d92b8d012702c6f473046d2ec2930a8922dc
MellyMel MeInyk <115183850+ed-u-cate21@users.noreply.github.com>
2022-10-07 11:13:44 -0400

    Update streamlit_app.py
    troubleshooting

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,5 +30,5 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display the fruityvice API response 
 import requests
-fruityvice_response = reqeusts.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
